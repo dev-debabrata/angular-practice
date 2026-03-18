6dedf2febf48500d6fa9b2f7ab03169b48ba921f
Debabrata Das <debabratadas711@gmail.com>
2026-03-18 22:17:55 +0530

    add header component

--- 01-easy-task/src/app/app.ts
+++ 01-easy-task/src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('01-easy-task');
+  
 }
