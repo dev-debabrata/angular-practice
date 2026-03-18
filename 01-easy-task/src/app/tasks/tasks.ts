6bdac70b218949e34420309a1154598c5a39d7b3
Debabrata Das <debabratadas711@gmail.com>
2026-03-18 22:37:16 +0530

    add task component

--- 01-easy-task/src/app/tasks/tasks.ts
+++ 01-easy-task/src/app/tasks/tasks.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { Task } from "./task/task";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [Task],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
